ca7558afa2471c591ab878f16b50ef39b2560368
soumya-gurjar8387 <soumya8387@gmail.com>
2026-08-22 12:43:53 +0530

    Add calculator project

---
+++ Calculator/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        CalculatorView view = new CalculatorView();
+        CalculatorModel model = new CalculatorModel();
+
+        new CalculatorController(view, model);
+    }
+}
